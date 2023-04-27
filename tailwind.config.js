5208c82b02f7c084cfdc8503156d459b70aafa50
Seshiru <ceciladam04@gmail.com>
2023-04-28 01:46:18 +0800

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,52 +1,73 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
-  ],
+  // mode: "jit",
+  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   theme: {
-    spacing: {
-      '0': '0',
-      '1': '0.25rem',
-      '2': '0.5rem',
-      '3': '0.75rem',
-      '4': '1rem',
-      '5': '1.25rem',
-      '6': '1.5rem',
-      '8': '2rem',
-      '10': '2.5rem',
-      '12': '3rem',
-      '16': '4rem',
-      '20': '5rem',
-      '24': '6rem',
-      '32': '8rem',
-      '40': '10rem',
-      '48': '12rem',
-      '56': '14rem',
-      '64': '16rem',
-    },
-    colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'red': '#FF0000',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#eee',
-      'gray-light': '#d3dce6',
-      'black': '#111'
-    },
-    fontFamily: {
-      serif: ['Inter', 'serif'],
-    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-        'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      
+      colors: {
+        'blue': '#1fb6ff',
+        'red':'#9F2A2A',
+        'white': '#FFFFFF',
+        'purple': '#7e5bef',
+        'black' : '#020808',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'blue': '#498EFF',
+        'green': '#13ce66',
+        'yellow': '#ffc82c',
+        'grey-dark': '#273444',
+        'grey': '#555',
+        'gray-light': '#d3dce6',
+        'lightgrey': '#E0E3E2',
+      },
+
+      text:{
+        'white': '#CDD0D5F',
+      },
+
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+      extend: {
+        spacing: {
+          '8xl': '96rem',
+          '9xl': '128rem',
+        },
+        borderRadius: {
+          '4xl': '2rem',
+        }
+      },
+      spacing: {
+        '1': '8px',
+        '2': '12px',
+        '3': '16px',
+        '4': '24px',
+        '5': '32px',
+        '6': '48px',
+      },
+
+      
+      // colors: {
+      //   hehe: '#18D039',
+      //   'great-blue': {
+      //     DEFAULT: '#2A669F',
+      //     50: '#E4F7F8',
+      //     100: '#CCEEF2',
+      //     200: '#9CD7E5',
+      //     300: '#6CB9D8',
+      //     400: '#3B94CB',
+      //     500: '#2A669F',
+      //     600: '#234B83',
+      //     700: '#1B3366',
+      //     800: '#14204A',
+      //     900: '#0C102E',
+      //     950: '#080A20',
+      //   },
+      // }
+      padding: {
+        63: '15.5rem',
       },
     },
   },
