38d84763217bd5ec421ffce6c20ce6e24027ceb3
Seshiru <ceciladam04@gmail.com>
2023-05-12 11:33:25 +0800

    last changes

--- src/components/topbar.js
+++ src/components/topbar.js
@@ -14,31 +14,31 @@ function Topbar() {
         <ul className="flex">
           <li><img src="govph.png" alt="" className="px-2 pb-1 transition-all hover:bg-red" /></li>
           <li className="">
-            <a href="#"></a>
+            {/* <a href="#"></a> */}
             <a><Link href="/pup" className="flex items-center transition-all bg-white-300 hover:bg-red font-I text-[10pt] hover:text-white text-gr" style={{ padding: '10px 16px' }}>Home</Link></a>
           </li>
           <li className="">
-            <a href="#"></a>
+            {/* <a href="#"></a> */}
             <a href="#" className="flex items-center transition-all bg-white-300 hover:bg-red font-I text-[10pt] hover:text-white text-gr hidden md:flex" style={{ padding: '10px 16px' }}>Good Governance</a>
           </li >
           <li className="">
-            <a href="#"></a>
+            {/* <a href="#"></a> */}
             <a href="#" className="flex items-center transition-all bg-white-300 hover:bg-red font-I text-[10pt] hover:text-white text-gr hidden md:flex" style={{ padding: '10px 16px' }}>Transparency</a>
           </li>
           <li className="">
-            <a href="#"></a>
+            {/* <a href="#"></a> */}
             <a><Link href="international" className="flex items-center transition-all bg-white-300 hover:bg-red font-I text-[10pt] hover:text-white text-gr hidden lg:flex" style={{ padding: '10px 16px' }}>International Affairs</Link></a>
           </li>
           <li className="">
-            <a href="#"></a>
+            {/* <a href="#"></a> */}
             <a href="#" className="flex items-center transition-all bg-white-300 hover:bg-red font-I text-[10pt] hover:text-white text-gr hidden md:flex" style={{ padding: '10px 16px' }}>Bid Notices and Invitation</a>
           </li>
           <div className="flex items-center">
-          <a href="#"></a>
+          {/* <a href="#"></a> */}
           <a href="#" className="text-xl font-bold tracking-wide"></a>
         </div>
         <div className="flex items-center mx-auto xl:pr-72 xl:pl-72">
-          <a href="#"></a>
+          {/* <a href="#"></a> */}
           <a><Link href="/contact_us" className="flex items-center transition-all text-gr bg-white-300 hover:bg-red hover:text-white font-I text-[10pt] py-2" style={{ padding: '10px 16px' }}>Contact Us</Link></a>
         </div>
         </ul>
